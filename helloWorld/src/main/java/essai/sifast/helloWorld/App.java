82824cb6ea7d1268c8545b1788153930a2b6c1d7
jiheneA <jihenee.abdelhedi@gmail.com>
2017-07-06 17:06:03 +0100

    helllo world java

---
+++ helloWorld/src/main/java/essai/sifast/helloWorld/App.java
@@ -0,0 +1,13 @@
+package essai.sifast.helloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
